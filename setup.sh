72660da3c67ea585c645242110c4fa8547e4bd6d
rinri <rin@rinri-d.xyz>
2020-11-15 10:15:11 +0000

    Added colored stdin, stdout and stderr

--- setup.sh
+++ setup.sh
@@ -10,9 +10,13 @@ read -p $'Enter starting line of cursor:\n' line
 ((line=line-1))
 echo "$line"gg > .gg
 
+# color.sh
+echo $'echo -en "\033[31m"  ## red\neval $* | while read line; do\necho -en "\033[36m"  ## blue\necho $line\necho -en "\033[31m"  ## red\ndone\necho -en "\033[0m"  ## reset color' > color.sh
+
 # run.sh
-echo $'g++ -std=c++14 -g $1\necho "Compiled..."\n./a.out\necho' > run.sh
+echo $'g++ -std=c++14 -g $1\necho "Compiled..."\n./color.sh ./a.out\necho' > run.sh
 
 # make all executable
 chmod +x create.sh
 chmod +x run.sh
+chmod +x color.sh
